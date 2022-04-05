6449afe9425edcec36f052d3f2c5f79b97b05a99
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-04-05 23:18:21 +0100

    Added type to bdpath

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 //Creating database when app starts... Create different database for test
-export let dbPath: any;
+export let dbPath: string;
 if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: './env.test' });
   dbPath = path.join(__dirname, '..', process.env.DBPATH!);
